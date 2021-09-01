1f4ce0793d97073cf95689fec82322f29e37f738
ppenguin <hieronymusv@gmail.com>
2021-09-01 22:24:00 +0200

    add DirSize() (du like)

--- gosysutils.go
+++ gosysutils.go
@@ -3,6 +3,7 @@ package gosysutils
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/shirou/gopsutil/disk"
 	"golang.org/x/sys/unix"
@@ -27,4 +28,47 @@ func FileFallocate(filepath string, size int64, mode os.FileMode, force bool) er
 // the latter may be useful for much more so we go with that
 func FsStatFromPath(path string) (*disk.UsageStat, error) {
 	return disk.Usage(path)
+}
+
+// DirSize returns the total size in bytes of the dir contents (recursively scanned)
+// Adapted from https://ispycode.com/Blog/golang/2017-01/DU-estimate-file-space-usage
+// work-alike with linux/unix du util
+func DirSize(currentPath string, info os.FileInfo) (int64, error) {
+	var err error
+
+	if info == nil {
+		info, err = os.Lstat(currentPath)
+		if err != nil {
+			return -1, err
+		}
+	}
+	size := info.Size()
+
+	if !info.IsDir() {
+		return size, nil
+	}
+
+	dir, err := os.Open(currentPath)
+	if err != nil {
+		return size, err
+	}
+	defer dir.Close()
+
+	fis, err := dir.Readdir(-1)
+	if err != nil {
+		return -1, err
+	}
+
+	for _, fi := range fis {
+		if fi.Name() == "." || fi.Name() == ".." {
+			continue
+		}
+		sizeinc, err := DirSize(filepath.Join(currentPath, fi.Name()), fi)
+		if err != nil {
+			return -1, err
+		}
+		size += sizeinc
+	}
+
+	return size, nil
 }
